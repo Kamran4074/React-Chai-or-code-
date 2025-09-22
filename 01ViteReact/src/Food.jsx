83e402f149fb2166e5cffb54f7dc4f4e6e367d6a
kamran alam <kammykamran0093@gmail.com>
2025-09-23 02:35:42 +0530

    first commit

---
+++ 01ViteReact/src/Food.jsx
@@ -0,0 +1,14 @@
+function Food(){
+    return(
+        <div>
+            <h1>This is Food Component</h1>
+            <ul>
+                <li>Apple</li>  
+                <li>Banana</li>
+                <li>Litchi</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Food;
